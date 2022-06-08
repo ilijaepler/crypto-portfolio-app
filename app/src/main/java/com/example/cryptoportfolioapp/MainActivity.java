ea498d3fed274fce8da401077a3c6078f2f6af2b
ilijaepler <eplerilija17@gmail.com>
2022-06-08 23:33:02 +0200

    Firebase authentication

--- app/src/main/java/com/example/cryptoportfolioapp/MainActivity.java
+++ app/src/main/java/com/example/cryptoportfolioapp/MainActivity.java
@@ -1,31 +1,80 @@
 package com.example.cryptoportfolioapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button buttonLogin;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
 
-        buttonLogin = findViewById(R.id.buttonLogin);
+        // Initialize Firebase Auth
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
+        // Redirecting to Login activity if the user is not authenticated
+        if(currentUser == null){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
+        TextView labelMainTitle = findViewById(R.id.labelMainTitle);
+
+        TextView labelMainLogout = findViewById(R.id.labelMainLogout);
+
+        // Logging out the user
+        labelMainLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                logoutUser();
             }
         });
+
+        // Getting database reference
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("users").child(currentUser.getUid());
+
+        // Getting the information about the user
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+
+                if(user != null){
+                    labelMainTitle.setText("Hello " + user.getFirstName() + ".");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {}
+        });
+    }
+
+    // Logging out the user
+    private void logoutUser(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/cryptoportfolioapp/RegisterActivity.java
+++ app/src/main/java/com/example/cryptoportfolioapp/RegisterActivity.java
@@ -1,27 +1,99 @@
 package com.example.cryptoportfolioapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_register);
 
-        TextView switchToLogin = findViewById(R.id.labelSwitchToLogin);
-        switchToLogin.setOnClickListener(new View.OnClickListener() {
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() != null){
+            finish();
+            return;
+        }
+
+        // Registration of the user
+        Button buttonRegister = findViewById(R.id.buttonRegister);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                switchToLogin();
+                registerUser();
             }
         });
+
+        // Switching to the login activity
+        TextView switchToLogin = findViewById(R.id.labelSwitchToLogin);
+        switchToLogin.setOnClickListener(view -> switchToLogin());
+    }
+
+    // Registration of the user
+    private void registerUser() {
+        EditText inputRegisterFirstName = findViewById(R.id.inputRegisterFirstName);
+        EditText inputRegisterLastName = findViewById(R.id.inputRegisterLastName);
+        EditText inputRegisterEmail = findViewById(R.id.inputRegisterEmail);
+        EditText inputRegisterPassword = findViewById(R.id.inputRegisterPassword);
+
+        String firstName = inputRegisterFirstName.getText().toString();
+        String lastName = inputRegisterLastName.getText().toString();
+        String email = inputRegisterEmail.getText().toString();
+        String password = inputRegisterPassword.getText().toString();
+
+        // Checking if the inputs are correct(Regex in the future)
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this, "Please fill all the fields!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Creation and registration of the user
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            User user = new User(firstName, lastName, email);
+                            FirebaseDatabase.getInstance().getReference("users")
+                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    showMainActivity();
+                                }
+                            });
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Authentication has failed.", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private void showMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     private void switchToLogin(){
